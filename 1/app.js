71ed7f5f5e10ebddd440800eff9c0ccddd53546c
Mass00 <krivmann00@gmail.com>
2022-06-06 16:30:57 +0200

    gemacht

--- 1/app.js
+++ 1/app.js
@@ -1,1 +1,11 @@
-alert("hello");
+let word = "apple";
+const vowels = 'aeiouy';
+let new_arr = [];
+let arr = word.toUpperCase().split('');
+for (let i = 0; i < arr.length; i++){
+    if( word.indexOf(vowels[i]) > -1){
+        new_arr.push(word.indexOf(vowels[i]) + 1);
+    }
+}
+
+console.log(new_arr);
