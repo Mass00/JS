465981f15b66a98bd137d8b47c42be10af09c050
Mass00 <krivmann00@gmail.com>
2022-06-07 10:55:31 +0200

    update 3

--- 3/app.js
+++ 3/app.js
@@ -1,9 +1,12 @@
 function multiplyAll(arr) {
     let product = 1;
     // Only change code below this line
-    product = arr.reduce(function(acc, cur) {
-        return acc.concat(cur);
-    }).reduce( (a, b) => a * b);
+    product = arr
+        .reduce(function (acc, cur) {
+            return acc.concat(cur);
+        })
+        .reduce((a, b) => a * b);
+        
     // Only change code above this line
     console.log(product);
     return product;
